be5fbc8d87a45407529941c1a547253baa6ae17c
newyearstag <robcoleman2343@gmail.com>
2023-02-15 18:43:15 -0500

    Lab 2
    Added Carousel, Cards, and updated Nav Bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavBarComponentComponent } from './navigation/top-nav-bar-component/top-nav-bar-component.component';
 import { FooterComponentComponent } from './navigation/footer-component/footer-component.component';
+import { CarouselComponent } from './body/carousel/carousel.component';
+import { CardsComponent } from './navigation/cards/cards.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavBarComponentComponent,
-    FooterComponentComponent
+    FooterComponentComponent,
+    CarouselComponent,
+    CardsComponent
+   
+    
+    
   ],
   imports: [
     BrowserModule,
